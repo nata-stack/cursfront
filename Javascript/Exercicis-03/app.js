c34d3b2ffa847289cb2957d0e2f6f9bfea8d64c1
nata-stack <naticujo@gmail.com>
2022-02-14 09:49:15 +0100

    ee

---
+++ Javascript/Exercicis-03/app.js
@@ -0,0 +1,5 @@
+/*
+1. Fes un programa on es forci el resultat NaN i es mostri per pantalla.
+2. Fes un programa per convertir un String nombre a nombre.
+3. Fes un programa per validar que no es un número.
+ */
